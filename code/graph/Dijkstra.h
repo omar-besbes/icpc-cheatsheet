7a2aaddc445674581746aaaf02131b98723ddb21
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2024-10-16 00:12:31 +0100

    new: Dijkstra

---
+++ code/graph/Dijkstra.h
@@ -0,0 +1,29 @@
+/**
+ * Shortest path
+ * T is the distance metric (e.g. int or long long).
+ */
+
+template <class node, typename D>
+struct Dijkstra {
+   vector<D> dist;
+   vector<vector<pair<int, D>>> adj;
+   Dijkstra(vector<vector<pair<int, D>>> &_adj) : adj(_adj) {}
+   void compute(int s) {
+      dist.assign(adj.size(), numeric_limits<D>::max());
+      typedef tuple<D, node> state;
+      priority_queue<state, vector<state>, greater<state>> q;
+      q.push({0, node(s)});
+      dist[s] = 0;
+      while (!q.empty()) {
+         auto [d, cur] = q.top();
+         q.pop();
+
+         for (auto [w, c] : adj[cur])
+            if (dist[c] > d + w) {
+               dist[c] = d + w;
+               q.push({dist[c], c});
+            }
+      }
+   }
+   D get(int idx) { return dist[idx]; }
+};
