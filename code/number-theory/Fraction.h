b76e9b1fae0bfa5f9cddba5dfdc421e541c60f47
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2024-12-05 02:06:49 +0100

    new: Data structures
    AVL Tree
    Lazy/Persistent Segment Tree
    Matrix

--- code/number-theory/Fraction.h
+++ code/number-theory/Fraction.h
@@ -1,11 +1,19 @@
+/**
+ * If fraction is negative, the nominator holds the sign.
+ * Nominator and denominator values can go up to 1e9.
+ * For values up to 1e18, replace ll with ld and int with ll
+ * and add the proper implementation of gcd for ld
+ */
+
 struct Frac {
   ll a, b;
-  Frac(ll a) : a(a), b(1) {}
-  Frac(ll x, ll y) {
+  Frac(int a) : a(a), b(1) {}
+  Frac(int x, int y) {
     assert(y != 0);
+    auto sgn = x && (x < 0) ^ (y < 0) ? -1 : 1;
+    x = abs(x), y = abs(y);  // ensure positive denominator
     auto g = gcd(x, y);
-    a = x / g, b = y / g;
-    if (b < 0) a *= -1, b *= -1; // ensure positive denominator
+    a = sgn * x / g, b = y / g;
   }
   Frac operator+(Frac f) { return Frac(a * f.b + b * f.a, b * f.b); }
   Frac operator-(Frac f) { return Frac(a * f.b - b * f.a, b * f.b); }
