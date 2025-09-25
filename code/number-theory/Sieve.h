48b3046d9cc3eb2611e62a36cff9011a9647b937
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2025-09-25 21:36:46 +0100

    fix: optimize spacing

--- code/number-theory/Sieve.h
+++ code/number-theory/Sieve.h
@@ -1,23 +1,17 @@
-const int N = 1e6;
-int lpd[N];
-vector<int> primes ; 
+vector<int> primes, lpd;
 vector<int> factorize(int n) {
    vector<int> ans;
-   while (n != 1) {
-      ans.push_back(lpd[n]);
-      n/= lpd[n];
-   }
+   while (n != 1) ans.push_back(lpd[n]), n/= lpd[n];
    return ans;
 }
 
-void sieve(){
+void sieve(int N) {
+   lpd.resize(N);
    for (int i = 2; i < N; i++) {
       if (lpd[i] == 0) {
-        primes.push_back(i) ;
-         for (int j = i; j < N; j+= i)
-            if (lpd[j] == 0)
-               lpd[j] = i;
+         primes.push_back(i);
+         for (int j = i; j < N; j += i)
+            if (lpd[j] == 0) lpd[j] = i;
       }
    }
 }
-
